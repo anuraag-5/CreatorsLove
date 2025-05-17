72bc0c6697d7fd1af44a9ff15b8c96e1d81e5a7c
Anurag bhoite <anuragcr7778@gmail.com>
2025-05-17 17:35:54 +0530

    perfect..

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import { useGSAP } from "@gsap/react";
+import Navbar from "@/components/Navbar";
+import { AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
 
 gsap.registerPlugin(useGSAP);
 
@@ -14,7 +17,6 @@ const Page = () => {
 
   useGSAP(
     (_, contextSafe) => {
-
       if (!ref2.current || !contextSafe) return;
 
       const handleMove = contextSafe((dets: MouseEvent) => {
@@ -27,33 +29,27 @@ const Page = () => {
           duration: 1,
           ease: "elastic.out(1,0.1)",
         });
-        
       });
 
       const handleLeave = contextSafe(() => {
-
-        const currentWidth = ref1.current?.offsetWidth || 0;
+        const currentWidth = ref1.current!.offsetWidth || 0;
 
         gsap.to("path", {
           attr: { d: `M 0 80 Q ${currentWidth / 2} 80 ${currentWidth} 80` },
           duration: 1,
           ease: "elastic.out(1,0.2)",
         });
-
       });
 
       ref2.current.addEventListener("mousemove", (dets) => handleMove(dets));
       ref2.current.addEventListener("mouseleave", () => handleLeave());
 
       return () => {
-
         ref2.current!.removeEventListener("mousemove", (dets) =>
           handleMove(dets)
         );
         ref2.current!.removeEventListener("mouseleave", () => handleLeave());
-
-      }
-
+      };
     },
     { dependencies: [] }
   );
@@ -67,55 +63,73 @@ const Page = () => {
     const creatorsLoveWidth = ref1.current.offsetWidth;
     setWidth(creatorsLoveWidth);
   }, []);
+
   return (
     <>
-      <div className="p-4 md:p-14 fullwh bg-conic">
-        <div className="w-full flex justify-between">
-          <Image
-            src="/creatorslove.svg"
-            alt="CreatorLove"
-            width={40}
-            height={40}
-            className=" object-cover"
-          />
-          <Image src="/menu-line.svg" alt="Menu" width={30} height={30} />
-        </div>
-        <div className="relative mt-28">
-          <div
-            className=" uppercase text-[24px] md:text-[45px] xl:text-[84px] w-fit tracking-wider"
-            ref={ref1}
+      <div className="p-4 md:p-14 fullwh bg-conic relative">
+        <Navbar />
+        <AnimatePresence>
+          <motion.div
+            className="relative mt-28 "
+            key="front"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 0.2,
+            }}
           >
-            Creatorslove
-          </div>
-          <svg
-            className={`absolute top-[-38px] md:top-[-16px] xl:top-9`}
-            style={{ width }}
-            ref={ref2}
-          >
-            <path
-              d={`M 0 80 Q ${width / 2} 80 ${width} 80`}
-              stroke="white"
-              fill="transparent"
-            />
-          </svg>
-          <div
-            className="flex justify-between items-center w-fit pl-3 border-[#a03794] border-[1px] mt-24 cursor-pointer"
-            onClick={() => {}}
-          >
-            <div className="uppercase text-[12px] text-[#a03794] mr-5 tracking-wider md:text-[16px]">
-              Get Started
+            <motion.div
+              className=" uppercase text-[24px] md:text-[45px] xl:text-[84px] w-fit tracking-wider"
+              ref={ref1}
+            >
+              Creatorslove
+            </motion.div>
+            <svg
+              className={`absolute top-[-38px] md:top-[-16px] xl:top-9`}
+              style={{ width }}
+              ref={ref2}
+            >
+              <path
+                d={`M 0 80 Q ${width / 2} 80 ${width} 80`}
+                stroke="white"
+                fill="transparent"
+              />
+            </svg>
+            <div
+              className="flex justify-between items-center w-fit pl-3 border-[#ff66c4] border-[1px] mt-24 cursor-pointer"
+              onClick={() => {}}
+            >
+              <div className="uppercase text-[12px] text-[#ff66c4] mr-5 tracking-wider md:text-[16px]">
+                Get Started
+              </div>
+              <Image
+                src="/arrow-right-s-line.svg"
+                alt="Click"
+                width={30}
+                height={30}
+              />
             </div>
-            <Image
-              src="/arrow-right-s-line.svg"
-              alt="Click"
-              width={30}
-              height={30}
-            />
-          </div>
-        </div>
-        <div className="text-[#a03794] w-fit h-fit mt-[215px] sm:mt-[140px]">
-          @unknown
-        </div>
+          </motion.div>
+          <motion.div
+            className="text-[#ff66c4] w-fit h-fit mt-[215px] sm:mt-[140px] font-sans"
+            key="username"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 0.2,
+            }}
+          >
+            @unknown
+          </motion.div>
+        </AnimatePresence>
       </div>
     </>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { AnimatePresence } from "motion/react";
+import { motion } from "motion/react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsOpenMenu(!isOpenMenu);
+  };
+
+  return (
+    <>
+      <div className="w-full flex justify-between sticky top-5 md:top-14 z-10">
+        <Link href="/">
+          <Image
+          src="/creatorslove.svg"
+          alt="CreatorLove"
+          width={35}
+          height={35}
+          />
+        </Link>
+        <AnimatePresence mode="wait">
+          {!isOpenMenu ? (
+            <motion.img
+              src="/menu-line.svg"
+              alt="Menu"
+              width={30}
+              height={30}
+              className="cursor-pointer"
+              onClick={() => handleMenuClick()}
+              key="menuopen"
+              layoutId="menu1"
+              initial="false"
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4 }}
+            />
+          ) : (
+            <motion.img
+              src="/close-large-line.svg"
+              alt="Menu"
+              width={30}
+              height={30}
+              className="cursor-pointer"
+              onClick={() => handleMenuClick()}
+              key="menuclose"
+              layoutId="menu1"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4, ease: "easeIn" }}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+      <AnimatePresence>
+        {isOpenMenu ? (
+          <motion.div
+            key="menu"
+            className="absolute top-0 right-0 left-0 bottom-0 bg-[rgba(0, 0, 0, 0.7)] backdrop-blur-sm z-9 flex flex-col justify-center items-center"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            exit={{
+              opacity: 0,
+            }}
+            transition={{
+              duration: 0.4,
+            }}
+          >
+            <motion.a
+              className="text-5xl mb-9 cursor-pointer"
+              onClick={handleMenuClick}
+              initial={{
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 0.4,
+              }}
+            >
+              HOME
+            </motion.a>
+            <motion.a
+              className="text-5xl mb-9 cursor-pointer"
+              href="/projects"
+              initial={{
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 0.4,
+              }}
+            >
+              PROJECTS
+            </motion.a>
+            <motion.a
+              className="text-5xl mb-9 cursor-pointer"
+              href="/contact"
+              initial={{
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                duration: 0.4,
+              }}
+            >
+              CONTACT
+            </motion.a>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default Navbar;

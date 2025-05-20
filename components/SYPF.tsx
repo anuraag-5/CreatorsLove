ab0bc9dc948d53ce439eda4c3e22f756b0a7001b
Anurag bhoite <anuragcr7778@gmail.com>
2025-05-21 01:06:49 +0530

    ...

--- components/SYPF.tsx
+++ components/SYPF.tsx
@@ -53,20 +53,20 @@ const SYPF = () => {
   );
 
   return (
-    <div className="fullwh relative flex flex-col justify-center items-center overflow-x-hidden" ref={ref1}>
-      <div className="text-9xl absolute top-20 left-50">
+    <div className="fullwh relative flex flex-col items-center overflow-x-hidden" ref={ref1}>
+      <div className="text-7xl md:text-9xl absolute top-25 md:top-10 left-5 md:left-25 lg:left-50">
         Sell
       </div>
-      <div className="text-[140px] absolute" ref={ref2}>
+      <div className="text-[70px] md:text-[140px] absolute top-[40%] md:top-[35%]" ref={ref2}>
         Clothes
       </div>
-      <div className="text-[140px] absolute clothes opacity-0" ref={ref3}>
+      <div className="text-[80px] md:text-[140px] absolute clothes opacity-0 top-[40%] md:top-[35%]" ref={ref3}>
         Clothes
       </div>
-      <div className="text-[140px] absolute clothes opacity-0" ref={ref4}>
+      <div className="text-[80px] md:text-[140px] absolute clothes opacity-0 top-[40%] md:top-[35%]" ref={ref4}>
         Clothes
       </div>
-      <div className="text-9xl absolute top-125 right-60">
+      <div className="text-7xl md:text-9xl absolute bottom-25 md:bottom-10 right-5 md:right-25 xl:right-50">
         Faster
       </div>
     </div>
